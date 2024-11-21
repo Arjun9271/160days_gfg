f308d78831502983b81424c78184c6175b7ce5b9
Arjun9271 <arjun.t1729@gmail.com>
2024-11-21 23:23:12 +0530

    gfg

---
+++ arrays/day_1/2nd largest element in array.py
@@ -0,0 +1,18 @@
+def second_largest_element(arr):
+    n = len(arr)
+    first = second = float("-inf")
+    if n < 2:
+        return -1
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+    if second == float("-inf"):
+        return -1
+    return second
+    
+value = second_largest_element([1,4,5,2,3])
+
+print(f"the second largest element is {value} ")
---
+++ arrays/day_2/pushZeros2end.py
@@ -0,0 +1,11 @@
+def pushZeros2end(arr):
+    n = len(arr)
+    count = 0
+    for i in range(n):
+        if arr[i] != 0:
+            arr[i],arr[count] = arr[count],arr[i]
+            count += 1
+    return arr
+
+modified_arr = pushZeros2end([1,2,0,3,0,5])
+print(modified_arr)
